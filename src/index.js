8118c99197df51d0bec600bc160c2fde0d1a3103
kielkow <matheuskiel@fiorifer.com.br>
2020-04-10 17:48:40 -0300

    HTTP Methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto1',
+    'Projeto2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto1',
+    'Projeto2',
+    'Projeto3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto4',
+    'Projeto2',
+    'Projeto3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto4',
+    'Projeto3',
+  ]);
 });
 
 app.listen(3333, () => {
